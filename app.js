e81d5de2da4de44d3b76564349739be0c623036b
akinbodebams <secretariat@abc.org.ng>
2022-06-08 16:36:50 +0100

    commencing with express

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "hello from the server side" });
+});
+const port = 3000;
+app.listen(port, () => {
+  console.log(`app running ${port}...`);
+});
