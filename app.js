b4843cfd421ac3210237cebb1523c4f4b935354c
akinbodebams <secretariat@abc.org.ng>
2022-06-10 00:58:46 +0100

    commencing with user data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const express = require("express");
-const res = require("express/lib/response");
+const morgan = require("morgan");
+
 const app = express();
 
 app.use(express.json());
@@ -9,8 +10,7 @@ const transactions = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/transactions.json`)
 );
 
-// For All Transactions in Database
-app.get("/api/v1/transactions", (req, res) => {
+const getAllTransactions = (req, res) => {
   res.status(200).json({
     status: "success",
     results: transactions.length,
@@ -18,10 +18,24 @@ app.get("/api/v1/transactions", (req, res) => {
       transactions,
     },
   });
-});
-
-// To add new Transaction to Database
-app.post("/api/v1/transactions", (req, res) => {
+};
+// To get single transacton from Database
+const getTransaction = (req, res) => {
+  const id = Number(req.params.id);
+  const transaction = transactions.find((el) => el.id === id);
+  if (!transaction) {
+    return res.status(404).json({
+      Status: "Fail",
+      Message: "Invalid Id",
+    });
+  }
+  res.status(200).json({
+    Status: "success",
+    transaction,
+  });
+};
+// to addd transaction to databsda
+const addTransaction = (req, res) => {
   const newId = transactions[transactions.length - 1].id + 1;
   const newTimestamp = Date.now();
   const newTransaction = Object.assign(
@@ -41,26 +55,9 @@ app.post("/api/v1/transactions", (req, res) => {
       });
     }
   );
-});
-
-// To get single transacton from Database
-app.get("/api/v1/transactions/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const transaction = transactions.find((el) => el.id === id);
-  if (!transaction) {
-    return res.status(404).json({
-      Status: "Fail",
-      Message: "Invalid Id",
-    });
-  }
-  res.status(200).json({
-    Status: "success",
-    transaction,
-  });
-});
-
+};
 // To Update a Specific Transaction
-app.patch("/api/v1/transactions/:id", (req, res) => {
+const updateTransaction = (req, res) => {
   id = Number(req.params.id);
   if (id > transactions.length) {
     return res.status(400).json({
@@ -72,7 +69,30 @@ app.patch("/api/v1/transactions/:id", (req, res) => {
     status: "Success",
     message: "Updated Successfully ",
   });
-});
+};
+//  to delete a transaction
+const deleteTransaction = (req, res) => {
+  id = Number(req.params.id);
+  if (id > transactions.length) {
+    return res.status(400).json({
+      Status: "Fail",
+      Message: "Invalid ID",
+    });
+  }
+  res.status(204).json({
+    status: "Success",
+    data: null,
+  });
+};
+
+// To get or creating new transaction
+app.route("/api/v1/transactions").get(getAllTransactions).post(addTransaction);
+
+app
+  .route("/api/v1/transactions/:id")
+  .get(getTransaction)
+  .patch(updateTransaction)
+  .delete(deleteTransaction);
 
 const port = 3000;
 app.listen(port, () => {
