64a73e84144011583e39c9dbd1603d1dc0fecf3a
akinbodebams <secretariat@abc.org.ng>
2022-06-12 23:31:38 +0100

    working fine

--- app.js
+++ app.js
@@ -18,4 +18,11 @@ const userRouter = require('./routes/userRoutes');
 app.use('/api/v1/transactions', transactionRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl.slice(1)} on this server`,
+  });
+});
+
 module.exports = app;
