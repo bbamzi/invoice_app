b627457e74031f9ae0a467e6b6d0faa97e9b6daf
akinbodebams <secretariat@abc.org.ng>
2022-06-13 00:19:35 +0100

    working fine

--- app.js
+++ app.js
@@ -19,11 +19,24 @@ app.use('/api/v1/transactions', transactionRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl.slice(1)} on this server`,
-  });
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl.slice(1)} on this server`,
+  //
+  const err = new Error(
+    `Can't find ${req.originalUrl.slice(1)} on this server`
+  );
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
 });
 
-const test = 'hi';
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 module.exports = app;
