71b72c0165f522c73455e6a0dd1eb0ae2be0f0b1
akinbodebams <bodejuniorforever@gmail.com>
2022-07-11 21:14:02 +0100

    Added error 404 page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const transactionRoute = require('./routes/transaction');
+const errorRoute = require('./routes/error');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
@@ -17,8 +18,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(transactionRoute);
+// app.use(errorRoute);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page Not Found</h1>');
+  res.status(404).render('404');
 });
 app.listen(3000);
