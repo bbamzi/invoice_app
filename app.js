26978c0c4bf90f032414dd330d40c889e4be94f1
akinbodebams <bodejuniorforever@gmail.com>
2022-07-11 16:50:29 +0100

    restarting app

--- app.js
+++ app.js
@@ -1,61 +1,22 @@
 // ###########################################################     Modules      #######################################
 const path = require('path');
+
 const express = require('express');
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController');
+const bodyParser = require('body-parser');
 const transactionRouter = require('./routes/transactionRoute');
-const userRouter = require('./routes/userRoutes');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const cookieParser = require('cookie-parser');
-const viewRouter = require('./routes/viewRoutes');
-// #######################################################      MiddleWares      ##################################################
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 
 const app = express();
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'public')));
-// set security http
-app.use(helmet());
-
-//  development logging
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-
-// limiting api requesy
-const limiter = rateLimit({
-  max: 30,
-  windowMs: 60 * 60 * 1000,
-  message: 'Too Many Request From This IP, Please Try Again In An Hour',
-});
-app.use('/api', limiter);
-
-// reading data from body into req.body
-app.use(express.json({ limit: '10kb' }));
-// app.use(cookieParser);
-
-// app.use((req, res, next) => {
-//   console.log(req.cookies);
-// });
-
-// data sanitization against nosql query injection
-app.use(mongoSanitize());
-// data sanitization agains xxs aTTACK
-app.use(xss());
-// serving stativ files
-
-app.use('/', viewRouter);
-app.use('/api/v1/transactions', transactionRouter);
-app.use('/api/v1/users', userRouter);
-
-app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+app.use('/', (req, res, next) => {
+  res.send(`<h1>inital page</>`);
 });
 
-app.use(globalErrorHandler);
-module.exports = app;
+app.use('/add-transaction', (req, res, next) => {
+  res.send(`<form>inital page</form>`);
+});
+
+app.listen(3000);
--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,52 +1,39 @@
-const factory = require('./handlerFactory');
-const Transaction = require('./../model/transactionModel');
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
+// const Transaction = require('./../model/transactionModel');
 
-exports.getAllTransactions = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.id) filter = { user: req.params.id };
-  const queryObj = { ...req.query };
-  const excludedFields = ['page', 'sort', 'limit', 'fields'];
-  excludedFields.forEach((el) => delete queryObj[el]);
-  const query = Transaction.find(queryObj).populate({
-    path: 'user',
-    select: 'firstName lastName',
-  });
-  const transactions = await query;
-  res.status(200).json({
-    status: 'success',
-    results: transactions.length,
-    data: {
-      transactions,
-    },
-  });
-});
-// To get single transacton from Database
-exports.getTransaction = factory.getOne(Transaction);
-
-// to add transaction to databsda
-exports.addTransaction = catchAsync(async (req, res, next) => {
-  if (!req.body.user) req.body.user = req.user.id;
-  const newTransaction = await Transaction.create(req.body);
-  res.status(201).json({
-    status: 'Success',
-    data: {
-      newTransaction,
-    },
-  });
-});
-// To Update a Specific Transaction
-exports.updateTransaction = factory.updateOne(Transaction);
-//  to delete a transaction
-exports.deleteTransaction = factory.deleteOne(Transaction);
-// exports.deleteTransaction = catchAsync(async (req, res, next) => {
-//   const transaction = await Transaction.findByIdAndDelete(req.params.id);
-//   if (!transaction) {
-//     return next(new AppError(`No Transaction with that ID found`, 404));
-//   }
-//   res.status(204).json({
-//     status: 'Success',
-//     data: null,
+// exports.getAllTransactions = catchAsync(async (req, res, next) => {
+//   let filter = {};
+//   if (req.params.id) filter = { user: req.params.id };
+//   const queryObj = { ...req.query };
+//   const excludedFields = ['page', 'sort', 'limit', 'fields'];
+//   excludedFields.forEach((el) => delete queryObj[el]);
+//   const query = Transaction.find(queryObj).populate({
+//     path: 'user',
+//     select: 'firstName lastName',
+//   });
+//   const transactions = await query;
+//   res.status(200).json({
+//     status: 'success',
+//     results: transactions.length,
+//     data: {
+//       transactions,
+//     },
 //   });
 // });
+// // To get single transacton from Database
+// exports.getTransaction = factory.getOne(Transaction);
+
+// // to add transaction to databsda
+// exports.addTransaction = catchAsync(async (req, res, next) => {
+//   if (!req.body.user) req.body.user = req.user.id;
+//   const newTransaction = await Transaction.create(req.body);
+//   res.status(201).json({
+//     status: 'Success',
+//     data: {
+//       newTransaction,
+//     },
+//   });
+// });
+
+// exports.updateTransaction = factory.updateOne(Transaction);
+
+// exports.deleteTransaction = factory.deleteOne(Transaction);
